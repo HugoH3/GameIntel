fbb61ca923f13f01706986d2d5e5c97e562ecac6
hugo3h <devhugoherrera@gmail.com>
2025-04-02 11:06:52 -0300

    feat: select boxes

--- app.py
+++ app.py
@@ -4,6 +4,29 @@ import plotly.express as px
 
 df = pd.read_csv('data/vg_sales.csv')
 df = df.drop(['img'], axis = 1)
-st.title('GameIntel Dashboard')
+df['release_date'] = pd.to_datetime(df['release_date'])
 
-st.dataframe(df)
+st.set_page_config(
+    page_title = 'GameIntel',
+    page_icon = "🎮",
+    layout = "wide"
+)
+genre = st.selectbox(
+    label = "Genre",
+    options = df['genre'].unique().tolist(),
+    index = None,
+    placeholder = "Select a Genre"
+)
+year  = st.selectbox(
+    label = "Year of release",
+    options = df['release_date'].dt.year.unique().tolist(),
+    index = None,
+    placeholder = "Select a Year of Release"
+)
+developer = st.selectbox(
+    label = "Developer",
+    options = df['developer'].unique().tolist(),
+    index = None,
+    placeholder = "Select a Developer"
+)
+st.title('Game Intel')
