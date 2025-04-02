7e8784b1c75fca4f2f8d8729b39cfa4c95977197
hugo3h <devhugoherrera@gmail.com>
2025-04-02 14:08:28 -0300

    feat: time interval slider

--- app.py
+++ app.py
@@ -6,27 +6,45 @@ df = pd.read_csv('data/vg_sales.csv')
 df = df.drop(['img'], axis = 1)
 df['release_date'] = pd.to_datetime(df['release_date'])
 
+def set_time_interval(initial, final):
+    time_filter = (df['release_date'].dt.year >= initial) & (df['release_date'].dt.year <= final)
+    return df[time_filter]
+
+#! Layout do Dashboard
 st.set_page_config(
     page_title = 'GameIntel',
     page_icon = "🎮",
     layout = "wide"
 )
-genre = st.selectbox(
+st.sidebar.header('Filtros')
+
+st.sidebar.multiselect(
     label = "Genre",
     options = df['genre'].unique().tolist(),
-    index = None,
-    placeholder = "Select a Genre"
+    placeholder = "Select one or more genres"
 )
-year  = st.selectbox(
-    label = "Year of release",
-    options = df['release_date'].dt.year.unique().tolist(),
-    index = None,
-    placeholder = "Select a Year of Release"
+publisher = st.sidebar.multiselect(
+    label = "Publisher",
+    options = df['publisher'].unique().tolist(),
+    placeholder = "Select one or more publishers"
 )
-developer = st.selectbox(
-    label = "Developer",
-    options = df['developer'].unique().tolist(),
-    index = None,
-    placeholder = "Select a Developer"
-)
+if st.sidebar.checkbox('Time Interval'):
+    years = st.sidebar.slider(
+        label = "Year",
+        min_value = df['release_date'].min().year,
+        max_value = df['release_date'].max().year,
+        value = (1980, 2020)
+    )
+    filtered_df = set_time_interval(years[0], years[1])
+else:
+    year = st.sidebar.slider(
+        label = "Year",
+        min_value=df['release_date'].min().year,
+        max_value=df['release_date'].max().year,
+        value=(1980)
+    )
+    filtered_df = set_time_interval(year, year)
+
+st.dataframe(filtered_df)
+
 st.title('Game Intel')
